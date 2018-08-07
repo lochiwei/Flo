bdc8839f8e896e713c1665978a2b7b520e368322
lochiwei <lochiwei.ios.dev.free@gmail.com>
2018-08-07 11:54:08 +0800

    finished "Core Graphics Tutorial - Part 1"

--- Flo/CounterView.swift
+++ Flo/CounterView.swift
@@ -12,31 +12,75 @@ import Extensions
 @IBDesignable
 class CounterView: UIView {
     
-    // constants
-    private enum Constant {
-        static let numberOfGlasses = 8
-        static let lineWidth = CGFloat(5)
-        static let arcWidth = CGFloat(76)
-        static var lineHalfWidth: CGFloat { return lineWidth / 2 }
-    }
+    // MARK: - Inspectables
     
-    // inspectable variables
+    @IBInspectable var numberOfGlasses = 8
+    
+    // counter arc
     @IBInspectable var counter: Int = 5
-    @IBInspectable var outlineColor: UIColor = .black
     @IBInspectable var counterColor: UIColor = .orange
+    @IBInspectable var counterWidth: CGFloat = 76
+    
+    // counter outline
+    @IBInspectable var outlineColor: UIColor = .black
+    @IBInspectable var outlineWidth: CGFloat = 5
+    
+    // MARK: - Design Details
+    
+    /// view's width and height
+    var w: CGFloat { return bounds.width }
+    var h: CGFloat { return bounds.height }
+    /// counter arc's center
+    var O: CGPoint { return bounds.center }
+    /// radius for counter arc
+    var r: CGFloat { return min(w, h)/2 - counterWidth/2 }
+    /// start angle for counter arc
+    let a1 = pi * 0.75
+    /// end angle for counter arc
+    let a2 = pi * 0.25
+    
+    // MARK: - Draw View
     
     // custom draw
     override func draw(_ rect: CGRect) {
+        drawCounterArc()
+        drawOutline()
+    }//end: draw()
+    
+    func drawCounterArc() {
         
-        // design dimensions
-        let (w, h) = (bounds.width, bounds.height)
-        let O = bounds.center
-        let r = max(w, h) / 2 - Constant.arcWidth / 2
-        let a1 = pi * 0.75      // start angle
-        let a2 = pi * 0.25      // end angle
         
-        // 1. draw path        
-        UIBezierPath.arc(center: O, radius: r, start: a1, end: a2, width: Constant.arcWidth, color: counterColor)
+        
+        // draw thick arc
+        UIBezierPath.arc(center: O, radius: r, start: a1, end: a2, width: counterWidth, color: counterColor)
+    }//end: drawCounterArc()
+    
+    func drawOutline() {
+        
+        // design outline path
+        let a3 = CGFloat(a1 + pi * 1.5 * counter.realValue / numberOfGlasses.realValue)
+        
+        let outline = UIBezierPath(
+            arcCenter: O,
+            radius: r + counterWidth/2 - outlineWidth/2,
+            startAngle: a1,
+            endAngle: a3,
+            clockwise: true
+        )
+        
+        // Note: when stroked, isolated subpaths will be connected automatically
+        outline.addArc(withCenter: O, radius: r - counterWidth/2 + outlineWidth/2, startAngle: a3, endAngle: a1, clockwise: false)
+        
+        outline.close()
+        
+        // draw outline
+        outline.strokeOnly(width: outlineWidth, color: outlineColor)
+    
+    }//end: drawOutline
+    
+    public func update(with n:Int) {
+        counter = n
+        setNeedsDisplay()
     }
     
-}
+}//end: CounterView
--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // data model
+    private var glasses: Int = 5 {
+        didSet {
+            // constraint to 0 ~ 8
+            guard 0 <= glasses && glasses <= 8 else {
+                glasses = oldValue
+                return
+            }
+            // update subviews when valid
+            updateViews()
+        }
+    }
+    
+    func updateViews() {
+        counterView.update(with: glasses)
+        counterLabel.text = "\(glasses)"
+    }
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var counterView: CounterView!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    // MARK: - Actions
+    
+    @IBAction func onButtonPressed(_ sender: PushButton)
+    {
+        glasses += sender.isAddButton ? 1 : -1
+    }
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateViews()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
